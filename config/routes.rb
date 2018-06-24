7b3e9160b2ef79b04baa74b8e3f214a4a588246a
Ryan MacGillivray <rmacg89@gmail.com>
2018-06-24 21:54:24 +0100

    Fix altered route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'application#index'
-  post '/access-code', to: 'rsvp#find_rsvp'
+  post '/access-code', to: 'rsvps#find_rsvp'
   resources :rsvps, only: :update
 end
