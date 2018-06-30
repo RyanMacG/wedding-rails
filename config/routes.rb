9e0708732285e87ac99b7ae51b1458419cddcec5
Ryan MacGillivray <rmacg89@gmail.com>
2018-06-30 14:45:13 +0100

    Super basic attending list (#6)

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'application#index'
   get '/rsvp', to: 'rsvps#landing', as: :rsvp_landing
   post '/access-code', to: 'rsvps#find_rsvp'
+  get '/who-is-coming', to: 'rsvps#attending_list'
   resources :rsvps, only: :update
 end
